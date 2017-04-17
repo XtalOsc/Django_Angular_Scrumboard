4ed399e78d0741fef84baad2f88e5fb9a3074759
Xtal Osc <xtalosc@gmx.com>
2017-04-17 12:07:59 -0500

    connected Card model to List, and added fields to Card

--- scrumboard/models.py
+++ scrumboard/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+#list of scrum cards
+class List(models.Model):
+    name =  models.CharField(max_length=50)
+
 #scrum card
 class Card(models.Model):
     title =  models.CharField(max_length=100)
     description = models.TextField(blank=True)
-
-#list of scrum cards
-class List(models.Model):
-    name =  models.CharField(max_length=50)
+    list_id = models.ForeignKey(List, related_name="cards")
+    story_points = models.IntegerField(null=True, blank=True)
+    business_value = models.IntegerField(null=True, blank=True)
+    
