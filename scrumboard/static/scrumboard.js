4bfc7b084e77108536852c026671a94625007c51
Xtal Osc <xtalosc@gmx.com>
2017-04-21 10:19:49 -0500

    can add new card

--- scrumboard/static/scrumboard.js
+++ scrumboard/static/scrumboard.js
@@ -7,17 +7,24 @@
 
     $scope.add = function(list, title){
       var card = {
+        list: list.id,
         title: title
       };//end var card
+      $http.post('/scrumboard/cards/', card)
+      .then(function(response){
+        //add card to list
+        list.cards.push(response.data);
+      },
+      //if error alert user
+      function(){
+        alert('Could not create card');
+      });//end $http.post
+    };//end $scope.add function
 
-      //add card to list
-      list.cards.push(card);
-    }//end $scope.add function
-
-$scope.data = [];
-$http.get('/scrumboard/lists').then(function(response){
-  $scope.data = response.data;
-});//end http.get response
+    $scope.data = [];
+    $http.get('/scrumboard/lists/').then(function(response){
+      $scope.data = response.data;
+    });//end http.get response
 
   }//end ScrumboardController()
 }());
