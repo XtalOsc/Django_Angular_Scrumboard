e2d79da1d46334d6f678f816dfc627e915176c89
Xtal Osc <xtalosc@gmx.com>
2017-04-23 15:49:53 -0500

    added ability to delete a card

--- scrumboard/static/card.directive.js
+++ scrumboard/static/card.directive.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-//retrieve module and add a directive
+  //retrieve module and add a directive
   angular.module('scrumboard.demo')
   .directive('scrumboardCard', CardDirective);
 
@@ -12,15 +12,33 @@
       restrict: 'E',
       controller: ['$scope','$http', function($scope, $http) {
         var url = 'scrumboard/cards/' + $scope.card.id + '/';
+
+        //Update card
         $scope.update = function() {
           $http.put(
             url,
             $scope.card
           );//end $http.put
         };//end $scope.update function
+
+        //Delete card
+        $scope.delete = function(){
+          $http.delete(url).then(
+            function(){
+              var cards = $scope.list.cards;
+              cards.splice(
+                cards.indexOf($scope.card),
+                1
+              );//end cards.splice
+            }//end function
+          );//end .then return
+        };//end $scope.delete function
+
+        //Debounce
         $scope.modelOptions = {
           debounce: 500
         };//end $scope.modelOptions
+
       }]//end controller
     };//end return object
   }//end CardDirective()
